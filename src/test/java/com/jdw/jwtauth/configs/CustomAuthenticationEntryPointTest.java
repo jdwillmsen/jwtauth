8dfbd0b20a513f29e06583f372500db731cf4012
Jake Willmsen <jdwillmsen@gmail.com>
2023-12-06 22:16:57 -0600

    Added tests

---
+++ src/test/java/com/jdw/jwtauth/configs/CustomAuthenticationEntryPointTest.java
@@ -0,0 +1,12 @@
+package com.jdw.jwtauth.configs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomAuthenticationEntryPointTest {
+
+    @Test
+    void commence() {
+    }
+}
